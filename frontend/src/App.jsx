770746fc0ec77bafebcd6e91657a5e22485790bd
saimanideep <saimanideep765@gmail.com>
2025-03-29 11:22:52 +0530

    1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
+import AllRoutes from './Allroutes'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <AllRoutes/>
+    </BrowserRouter>
+
+    </>
+
+  )
+}
+
+export default App
